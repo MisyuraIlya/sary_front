532abf62399c7660ea6597a1e37d55f18bf1ccfa
Ilya misyura <ilya.mi@digitrade.co.il>
2023-06-12 21:47:39 +0300

    add

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link ref='icon' href='/favicon.ico' sizes='any' type='image/svg+xml'/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
