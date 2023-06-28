f8f206ab498712cb09ad93e86c2f548c78176685
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2023-06-28 13:22:47 +0300

    add XL

--- src/components/AdminScreens/CreateModule/CreateModule.tsx
+++ src/components/AdminScreens/CreateModule/CreateModule.tsx
@@ -137,7 +137,7 @@ const CreateModule = () => {
                         <div className='rounded-x1 font-medium px-10 py-2 shadow bg-red text-white rounded-md cursor-pointer' onClick={() => ExerciseMethods.deleteModule(moduleId)}>מחיקה</div>                    
                       
                         <div className='justify-center items-center h-full flex'>
-                          <Image src={'/images/eye.svg'} width={25} height={25} alt='eye' className='cursor-pointer flex ' onClick={() =>  window.open(`http://3.74.228.194:4000/${exercises?.xl}`, '_blank', 'noopener')} />
+                          <Image src={'/images/xl.svg'} width={45} height={45} alt='xl' className='cursor-pointer flex ' onClick={() =>  window.open(`http://3.74.228.194:4000/${exercises?.xl}`, '_blank', 'noopener')} />
                         </div>
                       
                       </>
