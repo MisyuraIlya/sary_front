98b14362c8572d3990832d99615a5149fd07ca7c
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2023-09-20 11:30:50 +0300

    add

--- src/components/AdminScreens/CreateModule/Modules/SecondModule/helpers/ImageModuleHelper.ts
+++ src/components/AdminScreens/CreateModule/Modules/SecondModule/helpers/ImageModuleHelper.ts
@@ -4,7 +4,7 @@ export const getImageValue = (data: ISecondModuleSubExercises[]) => {
     const values = data.flatMap((item) => {
         const collectionsRows = item.collectionsRows;
         const allRows = collectionsRows.flatMap((item2) => item2.collectionRow);
-        const filteredRows = allRows.filter((item3) => item3.module_type ===  ('imageLeft' || 'imageRight'));
+        const filteredRows = allRows.filter((item3) => item3?.module_type ===  ('imageLeft' || 'imageRight'));
         const allValues = filteredRows.flatMap((item3) => item3.collectionValues);
         return allValues.flatMap((item4) => item4.value);
       });
@@ -16,7 +16,7 @@ export const getImageId = (data: ISecondModuleSubExercises[]) => {
     const values = data.flatMap((item) => {
         const collectionsRows = item.collectionsRows;
         const allRows = collectionsRows.flatMap((item2) => item2.collectionRow);
-        const filteredRows = allRows.filter((item3) => item3.module_type ===  ('imageLeft' || 'imageRight'));
+        const filteredRows = allRows.filter((item3) => item3?.module_type ===  ('imageLeft' || 'imageRight'));
         return filteredRows.flatMap((item3) => item3.id);
       });
 
